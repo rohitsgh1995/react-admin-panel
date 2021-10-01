f8044a3e6af1dca0a2190921b7a2401827627f99
unknown <rohit1995sgh@gmail.com>
2021-10-01 17:23:22 +0530

    topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./topbar.css"
+import { NotificationsNone } from '@material-ui/icons';
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">Alo Doc</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
